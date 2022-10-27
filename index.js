8b78740d82eb7192625d795c98e946a1f27674e8
rakib38324 <rakib3832@gmail.com>
2022-10-27 20:10:00 +0600

    Commit the all courses information

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 
- const Courses = require('./Data/Courses.json')
+ const Courses = require('./Data/courses.json')
+ const allcourses = require('./Data/allcourses.json')
 
 
  app.get('/', (req, res) =>{
@@ -16,6 +17,29 @@ app.use(cors())
 
  app.get('/Courses', (req, res)=>{
     res.send(Courses)
+ })
+
+ app.get('/allcourses/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = allcourses.find(course => course._id === id);
+    res.send(selectedCourse);
+ })
+ app.get('/course/:id', (req, res)=>{
+    const id = req.params.id;
+    const CourseId = allcourses.find(course => course._id === id);
+    res.send(CourseId);
+ })
+
+ app.get('/catagory/:id',(req, res)=>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(allcourses)
+    }
+    else{
+        const selectedCatagory= allcourses.filter(course => course.category_id === id);
+        res.send(selectedCatagory);
+    }
+    
  })
 
  app.listen(port, ()=>{
