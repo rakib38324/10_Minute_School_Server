3e90e415dd59df5363e748f9fa41e397912e1c6a
rakib38324 <rakib3832@gmail.com>
2022-10-27 20:16:01 +0600

    Commit the index.js file indentation

--- index.js
+++ index.js
@@ -3,45 +3,44 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
 app.use(cors())
 
+const Courses = require('./Data/courses.json')
+const allcourses = require('./Data/allcourses.json')
 
- const Courses = require('./Data/courses.json')
- const allcourses = require('./Data/allcourses.json')
-
-
- app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("Courses API running")
- });
+});
 
- app.get('/Courses', (req, res)=>{
+app.get('/Courses', (req, res) => {
     res.send(Courses)
- })
+});
 
- app.get('/allcourses/:id', (req, res)=>{
+app.get('/allcourses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = allcourses.find(course => course._id === id);
     res.send(selectedCourse);
- })
- app.get('/course/:id', (req, res)=>{
+});
+
+app.get('/course/:id', (req, res) => {
     const id = req.params.id;
     const CourseId = allcourses.find(course => course._id === id);
     res.send(CourseId);
- })
+});
 
- app.get('/catagory/:id',(req, res)=>{
+app.get('/catagory/:id', (req, res) => {
     const id = req.params.id;
-    if(id === '08'){
+    if (id === '08') {
         res.send(allcourses)
     }
-    else{
-        const selectedCatagory= allcourses.filter(course => course.category_id === id);
+    else {
+        const selectedCatagory = allcourses.filter(course => course.category_id === id);
         res.send(selectedCatagory);
     }
-    
- })
 
- app.listen(port, ()=>{
-    console.log("Running",port)
- } )
+});
+
+
+app.listen(port, () => {
+    console.log("Running", port)
+})
